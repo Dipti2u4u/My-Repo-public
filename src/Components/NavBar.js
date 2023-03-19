8a37c0764a7de550ec8e1cae8ef28e63a69f8e40
Dipti Prasad Das <diptiprasaddas50@gmail.com>
2023-03-19 22:50:41 +0530

    Navbar project in React

---
+++ src/Components/NavBar.js
@@ -0,0 +1,23 @@
+import './NavBar.css' ;
+
+function NavBar() {
+    
+    return(
+        <div className = "header">
+            <div className = "Logo">NavBar</div>
+            <div className = "links">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        </div>
+
+
+    );
+
+}
+
+export default NavBar ;
